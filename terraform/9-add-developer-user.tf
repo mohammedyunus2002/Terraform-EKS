084b1e8b48623515c85e4342076cc851d629cf7e
Mohammed Yunus Shaikh <shaikhyunus.us89@gmail.com>
2024-12-17 12:10:25 +0530

    Added helm provider, metrics server, cluster autoscaler and aws loadbalancer iwth k8s manifest files

--- terraform/9-add-developer-user.tf
+++ terraform/9-add-developer-user.tf
@@ -1,30 +1,33 @@
 resource "aws_iam_user" "developer" {
   name = "developer"
 }
-
-resource "aws_iam_policy" "developer_eks" {
-    name = "AmazonEKSDeveloperPolicy"
-
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "eks:DescribeCluster",
-                "eks:ListClusters"
-            ],
-            "Resource": "*"
-        }
-    ]
+data "aws_iam_policy" "existing_developer_eks" {
+  name = "AmazonEKSDeveloperPolicy"
 }
-POLICY
-}
+
+# resource "aws_iam_policy" "developer_eks" {
+#     name = "AmazonEKSDeveloperPolicy"
+
+#   policy = <<POLICY
+# {
+#     "Version": "2012-10-17",  
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "eks:DescribeCluster",
+#                 "eks:ListClusters"
+#             ],
+#             "Resource": "*"
+#         }
+#     ]
+# }
+# POLICY
+# }
 
 resource "aws_iam_user_policy_attachment" "developer_eks" {
   user = aws_iam_user.developer.name
-  policy_arn = aws_iam_policy.developer_eks.arn
+  policy_arn = data.aws_iam_policy.existing_developer_eks.arn
 }
 
 resource "aws_eks_access_entry" "developer" {
